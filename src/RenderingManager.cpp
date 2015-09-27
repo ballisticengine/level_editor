c7e6e13531ae912d7d0e26c6c924a26969247cb7
maciek <maciej.zakrzewski87@gmail.com>
2015-09-27 18:25:25 +0200

    ui setup

--- src/RenderingManager.cpp
+++ src/RenderingManager.cpp
@@ -1,6 +1,12 @@
 #include "RenderingManager.hpp"
 
+#include "ui/ui.hpp"
 
 void LeveledRenderingManager::render() {
-    
+    this->renderer->beforeFrame();
+    this->renderer->resetMatrix();
+    UI::getInstance()->getContext()->Render();
+    UI::getInstance()->getContext()->Update();
+    this->renderer->afterFrame();
+    this->flush_callback();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,43 @@ using namespace std;
 #include "config/EngineState.hpp"
 #include "libload/LibLoad.hpp"
 #include "renderer/RendererInterface.hpp"
+#include "ui/librocket_interfaces/RendererInterfaceSDL2.hpp"
+#include "ui/librocket_interfaces/ShellFileInterface.hpp"
+#include "ui/librocket_interfaces/SystemInterfaceSDL2.hpp"
+#include "ui/ui.hpp"
 
 #include "IO.hpp"
 #include "RenderingManager.hpp"
 
 int main() {
     cout << "Ballistic Engine Level Editor" << endl;
-    LibLoad::getInstance()->setWD("../ballistic_engine/bin");
-    LibLoad::getInstance()->discoverLoaders();
-    LibLoad::getInstance()->registerModule("renderer2", "RendererOpenGL", "returnRenderer");
-    
-    RendererInterface *ri = (RendererInterface *) LibLoad::getInstance()->getModuleClass("renderer2");
-    LeveledRenderingManager *rm = LeveledRenderingManager::getInstance();
-    rm->setRenderer(ri);
-    
     
     EngineState::getInstance()->setBool("exit", false);
+    EngineState::getInstance()->setString("engine_wd", "../ballistic_engine");
+    
+    LibLoad::getInstance()->setWD(EngineState::getInstance()->getString("engine_wd") + "/bin");
+    LibLoad::getInstance()->discoverLoaders();
+    LibLoad::getInstance()->registerModule("renderer2", "RendererOpenGL", "returnRenderer");
+
+    string ui_dir = "./data/ui/";
+    
+    RendererInterface *ri = (RendererInterface *) LibLoad::getInstance()->getModuleClass("renderer2");
+    ri->init(800, 600, IO::getInstance());
+    LeveledRenderingManager *rm = LeveledRenderingManager::getInstance();
+    rm->setRenderer(ri);
+    rm->setFlush(IO::flush);
+    Rocket::Core::RenderInterface  *ui_renderer = ri->getUiRenderer();
+    RocketSDL2SystemInterface *system_interface = new RocketSDL2SystemInterface();
+    Rocket::Core::FileInterface *file_interface = new ShellFileInterface(ui_dir.c_str());
+
+    UI *ui = UI::getInstance();
+
+    if (!ui->init(system_interface, ui_renderer, file_interface, rm->getRenderer())) {
+        cout << "UI loading error" << endl;
+    } else {
+        cout << "UI loaded!" << endl;
+    }
+    
     IO *io = IO::getInstance();
     io->init(io);
     io->eventLoop();
