bac6e6109b237bb8c46ef609fd721c2e11fd880e
maciek <maciej.zakrzewski87@gmail.com>
2015-11-01 18:46:42 +0100

    level saving

--- src/listeners/SaveListener.cpp
+++ src/listeners/SaveListener.cpp
@@ -1,4 +1,8 @@
 #include "SaveListener.hpp"
+#include "libload/LibLoad.hpp"
+#include "resources/WorldLoader.hpp"
+#include "world/WorldManager.hpp"
+#include <boost/filesystem.hpp>
 
 SaveListener::SaveListener() {
     Rocket::Core::ElementDocument *doc = ui->addDocument("save_dialog.rml", "save-dialog")->getDocument();
@@ -10,11 +14,21 @@ SaveListener::SaveListener() {
 void SaveListener::ProcessEvent(Rocket::Core::Event& event) {
     string id = string(event.GetTargetElement()->GetId().CString());
 
+    WorldLoader *l = (WorldLoader *) LibLoad::getInstance()->getLoaderByExtension("xml", LEVEL);
+
     if (id == "ok") {
         Rocket::Controls::ElementFormControlInput *el = (Rocket::Controls::ElementFormControlInput*)
                 ui->getDocument("save-dialog")->getElementById("save-level-name");
-        
+
         cout << "Save into" << el->GetValue().CString() << endl;
+        
+        string save_path = EngineState::getInstance()->getString("engine_wd") + 
+                "/data/levels/" + el->GetValue().CString();
+        string save_fn =  save_path + "/level.xml";
+        
+        boost::filesystem::create_directories(save_path);
+        
+        l->save(WorldManager::getInstance()->getCurrentWorld(), save_fn);
         ui->getDocument("save-dialog")->hide();
     }
 
